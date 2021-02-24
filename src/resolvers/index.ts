2136ed42484acb8f6cdfa188064ee78de908e2d8
AmoDinho <u14284783@tuks.co.za>
2021-02-24 23:19:39 +0200

    exported getAUser

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,9 @@
-import { hello, getUsers } from "./query";
+import { hello, getUsers, getAUser } from "./query";
 import { IResolvers } from "graphql-tools/dist/Interfaces";
 export const resolvers: IResolvers = {
   Query: {
     hello: (root: any, args: any, context: any) => hello(args, context),
     getUsers: (root: any, args: any, context: any) => getUsers(args, context),
+    getAUser: (root: any, args: any, context: any) => getAUser(args, context),
   },
 };
