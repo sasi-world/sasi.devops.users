7f8a7b2f8f87e1c08ffafaee0aefd93704c7d18f
AmoDinho <u14284783@tuks.co.za>
2021-02-24 22:56:23 +0200

    created all users query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,7 +1,15 @@
+import * as dynamoDB from "../../libs/dynamoDB";
 export const hello = (args: any, context: any) => {
   return "Your GraphQL API is now LIVE!🎈 ";
 };
 
-export const getUsers = (args: any, context: any) => {
-  return null;
+export const getUsers = async (args: any, context: any) => {
+  const params: object = {
+    TableName: process.env.UsersDB,
+  };
+  try {
+    const users = await dynamoDB.default.scan(params);
+  } catch (e) {
+    throw new Error(e);
+  }
 };
